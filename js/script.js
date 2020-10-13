f41cb5effb639a5a336613e2008fae3bb42d9e06
Madeuss <mateus1two3@gmail.com>
2020-10-13 17:20:15 -0300

    testing git flow

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function stylingSection() {
+  let section = document.getElementById("section");
+  section.classList.add("styled-section");
+
+  let text = document.getElementById("helloWorld");
+  text.classList.add("colored-text");
+}
+stylingSection()
